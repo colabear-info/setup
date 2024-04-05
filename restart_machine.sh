1b4f8a9f6756be7d23026b27494714961dc47453
Cola Bear <101950942+colabear-info@users.noreply.github.com>
2024-04-05 16:24:04 -0700

    Create restart_machine.sh

---
+++ restart_machine.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt-get update
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release vim zsh git wget  tmux  git-lfs
+
+export HOME=/workspace
+
+cd $HOME
+
+zsh
+
