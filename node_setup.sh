60ca07fd7ae06ddf2ac0b6d5fa42004f25790aea
Cola Bear <101950942+colabear-info@users.noreply.github.com>
2024-04-06 14:26:11 -0700

    Update node_setup.sh

--- node_setup.sh
+++ node_setup.sh
@@ -1,2 +1,4 @@
 
 git clone https://github.com/Fannovel16/ComfyUI-Video-Matting.git
+git clone https://github.com/ltdrdata/ComfyUI-Manager.git
+git clone https://github.com/alt-key-project/comfyui-dream-video-batches.git
